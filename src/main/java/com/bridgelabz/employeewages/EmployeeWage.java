b59edc5c8d6cdae02b73c3eaa2ab1474b2989b16
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-16 10:54:04 -0800

    Added:Checking Employee Daily Wage

--- src/main/java/com/bridgelabz/employeewages/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewages/EmployeeWage.java
@@ -2,13 +2,10 @@ package com.bridgelabz.employeewages;
 
 public class EmployeeWage {
     public static void main(String args[]){
-        double code = Math.floor(Math.random()*10)%2;
-        if(code==1){
-            System.out.println("Employee is Present");
-        }
-        else {
-            System.out.println("Employee is Absent");
-        }
+        int fullyDayHours = 8;
+        int wagePerHour = 20;
+        int dailyWage = (fullyDayHours*wagePerHour);
+
+        System.out.println("employee is present and his daily wage is "+dailyWage);
     }
 }
-
