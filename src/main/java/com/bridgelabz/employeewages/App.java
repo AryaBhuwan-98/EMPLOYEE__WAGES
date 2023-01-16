b6a5a37e0a4b1c6b44cb09e0e203d3cf3f9d74e2
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-16 10:41:47 -0800

    Added:WELCOME MESSAGE FOR EMPLOYEE WAGE PROGRAM

---
+++ src/main/java/com/bridgelabz/employeewages/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeewages;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome TO EMPLOYEE WAGE " );
+    }
+}
